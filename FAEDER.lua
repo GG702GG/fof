b971a8c032d8dbcd5f33c47257282fe7c7089daf
laksis1500 <64811139+laksis1500@users.noreply.github.com>
2021-07-10 08:59:28 -0700

    Update FAEDER.lua

--- FAEDER.lua
+++ FAEDER.lua
@@ -3386,7 +3386,7 @@ else
 if not faederdx1:get(FAEDER.."bot:tt:link:"..msg.chat_id_) then 
 local link = faederdx1:get(FAEDER.."bot:group:link"..msg.chat_id_)
 if link then
-faederdx(msg.chat_id_, msg.id_, 1, "❍ *رابط المجموعة* .\n\n" .. link, 1, "md")
+faederdx(msg.chat_id_, msg.id_, 1, "❍[رابط المجموعة] .\n\n" (.. link, 1,) "md")
 else 
 faederdx(msg.chat_id_, msg.id_, 1, '❍ لم يتم حفظ رابط المجموعه ارسل لي (ضع رابط) ليتم حفظه 📥 ،', 1, 'md')
 end else
