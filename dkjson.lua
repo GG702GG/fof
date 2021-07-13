2435afc5b5eae17d65609f0af4fb038ae5b43c8e
laksis1500 <64811139+laksis1500@users.noreply.github.com>
2021-07-13 07:17:28 -0700

    Update dkjson.lua

--- dkjson.lua
+++ dkjson.lua
@@ -297,45 +297,6 @@ end
 do
     local monitor_loop = function(client)
         local monitoring = true
-
-        -- Tricky since the payload format changed starting from Redis 2.6.
-        local pattern = '^(%d+%.%d+)( ?.- ?) ?"(%a+)" ?(.-)$'
-
-        local abort = function()
-            monitoring = false
-        end
-
-        return coroutine.wrap(function()
-            client:monitor()
-
-            while monitoring do
-                local message, matched
-                local response = response.read(client)
-
-                local ok = response:gsub(pattern, function(time, info, cmd, args)
-                    message = {
-                        timestamp = tonumber(time),
-                        client    = info:match('%d+.%d+.%d+.%d+:%d+'),
-                        database  = tonumber(info:match('%d+')) or 0,
-                        command   = cmd,
-                        arguments = args:match('.+'),
-                    }
-                    matched = true
-                end)
-
-                if not matched then
-                    client.error('Unable to match MONITOR payload: '..response)
-                end
-
-                coroutine.yield(message, abort)
-            end
-        end)
-    end
-
-    client_prototype.monitor_messages = function(client)
-        return monitor_loop(client)
-    end
-end
 -- locale independent num2str and str2num functions
 local decpoint, numfilter
 
